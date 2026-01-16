a4d51ca2784835b1a21a4b14431c2c10ce0beca5
Xiaoyi Chen <126677643+xxc451@users.noreply.github.com>
2026-01-17 00:19:32 +0100

    Add functionalities to calculator buttons

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(x, y) {
-    return x + y;
+    return (x + y).toString();
 }
 
 function subtract(x, y) {
-    return x - y;
+    return (x - y).toString();
 }
 
 function multiply(x, y) {
-    return x * y;
+    return (x * y).toString();
 }
 
 function divide(x, y) {
-    return x / y;
+    return (x / y).toString();
 }
 
 function operate(x, y, op) {
@@ -28,3 +28,105 @@ function operate(x, y, op) {
             alert("Operator Error");
     }
 }
+
+let firstNumber = "0";
+let secondNumber = "";
+let operator;
+let operatorPressed = false;
+let equalPressed = false;
+
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
+const sign = document.querySelector("#sign");
+const decimal = document.querySelector("#decimal");
+
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if (operatorPressed) {
+            if (secondNumber === "0") {
+                secondNumber = number.textContent;
+                display.textContent = secondNumber;
+            } else {
+                secondNumber += number.textContent;
+                display.textContent = secondNumber;
+            }
+        } else {
+            if (equalPressed || firstNumber === "0") {
+                firstNumber = number.textContent;
+                display.textContent = firstNumber;
+                equalPressed = false;
+            } else {
+                firstNumber += number.textContent;
+                display.textContent = firstNumber;
+            }
+        }
+        
+    });
+});
+
+operators.forEach(op => {
+    op.addEventListener("click", () => {
+        const prevOperator = operator;
+        operator = op.textContent;
+        if (operatorPressed && secondNumber) {
+            firstNumber = operate(firstNumber, secondNumber, prevOperator);
+            display.textContent = firstNumber;
+            secondNumber = "";
+        } else {
+            operatorPressed = true;
+            secondNumber = "";
+        }
+    });
+});
+
+equal.addEventListener("click", () => {
+    if (operatorPressed || operator) {
+        if (!secondNumber) {
+            secondNumber = firstNumber;
+        }
+        firstNumber = operate(firstNumber, secondNumber, operator);
+        display.textContent = firstNumber;
+        operatorPressed = false;
+    }
+    equalPressed = true;
+});
+
+clear.addEventListener("click", () => {
+    firstNumber = "0";
+    secondNumber = "";
+    operator = null;
+    operatorPressed = false;
+    equalPressed = false;
+    display.textContent = firstNumber;
+});
+
+sign.addEventListener("click", () => {
+    if (operatorPressed) {
+        if (secondNumber && secondNumber !== "0") {
+            secondNumber = secondNumber.at(0) === "-" ? secondNumber.slice(1) : "-" + secondNumber;
+            display.textContent = secondNumber;
+        }
+    } else {
+        if (firstNumber !== "0") {
+            firstNumber = firstNumber.at(0) === "-" ? firstNumber.slice(1) : "-" + firstNumber;
+            display.textContent = firstNumber;
+        }
+    }
+});
+
+decimal.addEventListener("click", () => {
+    if (operatorPressed) {
+        if (!secondNumber.includes(".")) {
+            secondNumber = secondNumber === "" ? "0." : secondNumber + ".";
+            display.textContent = secondNumber;
+        }
+    } else {
+        if (!firstNumber.includes(".")) {
+            firstNumber = firstNumber + ".";
+            display.textContent = firstNumber;
+        }
+    }
+});
