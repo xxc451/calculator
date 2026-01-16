967e21bb28340a0b332003a336dd6e433738fad1
Xiaoyi Chen <126677643+xxc451@users.noreply.github.com>
2026-01-16 01:02:55 +0100

    Add basic calculator operator functions

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(x, y, op) {
+    switch (op) {
+        case "+":
+            return add(Number(x), Number(y));
+        case "-":
+            return subtract(Number(x), Number(y));
+        case "×":
+            return multiply(Number(x), Number(y));
+        case "÷":
+            return divide(Number(x), Number(y));
+        default:
+            alert("Operator Error");
+    }
+}
